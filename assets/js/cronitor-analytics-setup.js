0013049eb7bfb3fbf1cc463c064bd78866fb289c
jpsaha <jpsaha@users.noreply.github.com>
2025-07-23 18:52:15 +0000

    Deploying to gh-pages from @ jpsaha/mo@d3fc1505180c065b4838cfead606812e24a0062d 🚀

--- assets/js/cronitor-analytics-setup.js
+++ assets/js/cronitor-analytics-setup.js
@@ -3,4 +3,4 @@ window.cronitor =
   function () {
     (window.cronitor.q = window.cronitor.q || []).push(arguments);
   };
-cronitor("config", { clientKey: "" });
+cronitor("config", { clientKey: "fa6a83dcadeb52c7364e6c3392133a40" });
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,43 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/mo/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/mo/projects/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/mo/cv/";
-          },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-a-simple-inline-announcement",
+        },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
@@ -109,6 +73,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/mo/projects/9_project/";
             },},{
+        id: 'social-discord',
+        title: 'Discord Server',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://discord.gg/55adEhUWdK", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -130,17 +101,17 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/jpsaha", "_blank");
         },
       },{
+        id: 'social-mathstodon',
+        title: 'Mathstodon',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://mathstodon.xyz/@jpsaha", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
           window.open("/mo/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },];
